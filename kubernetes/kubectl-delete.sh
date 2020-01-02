5bedab9af0abd90f51f3e8e10659ac08b4d95b6c
caokai2 <caokai2@lenovo.com>
2020-01-02 13:51:47 +0800

    add kubectl-delete.sh

---
+++ kubernetes/kubectl-delete.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
+# Usage: bash kubectl-delete.sh
+
+logSummary(){
+    echo ""
+    echo "#####################################################"
+    echo "Please find the below useful endpoints,"
+    echo "#####################################################"
+}
+
+kubectl delete -f registry/
+kubectl delete -f gateway/
+kubectl delete -f blog/
+kubectl delete -f store/
+
+logSummary
