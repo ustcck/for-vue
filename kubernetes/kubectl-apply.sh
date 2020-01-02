bd9efd2ced5902f3c8bc5e2c1617dc9f9ff28608
caokai2 <caokai2@lenovo.com>
2020-01-02 13:49:54 +0800

    first commit: add .gitignore and README.md

---
+++ kubernetes/kubectl-apply.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
+# Usage: bash kubectl-apply.sh
+
+logSummary(){
+    echo ""
+    echo "#####################################################"
+    echo "Please find the below useful endpoints,"
+    echo "#####################################################"
+}
+
+kubectl apply -f registry/
+kubectl apply -f gateway/
+kubectl apply -f blog/
+kubectl apply -f store/
+
+logSummary
